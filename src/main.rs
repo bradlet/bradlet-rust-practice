a0b93376df3ea2a8a2a1473a1aba331994134cf6
bradlet <bradlet2@pdx.edu>
2022-04-09 16:24:03 -0700

    Start off w/ program selection still getting using to ownership

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
-/*
-    ### PracticeRust
-    ##### Bradley Thompson
-    A project that I will use to keep track of my practice programs as I acquaint myself with Rust.
- */
+/// PracticeRust
+/// @author bradlet
+
+/// A project that I will use to keep track of my practice programs as I acquaint myself with Rust.
+mod models;
 
 use std::env::Args;
+use std::fmt::Debug;
+use models::types;
+use crate::types::Program;
 
 fn main() {
-    let mut args: Args = std::env::args();
+    let args: Vec<String> = std::env::args().collect();
     if args.len() != 2 { panic!("Invalid arguments provided") }
 
     // Note to self: args.nth(n) is destructive and drops all parsed elements in the iterator
     //  Also: 0th element is the name of this program.
-    let input = args.nth(1).expect("No CLI input provided.");
-    println!("Hello, world!\nArg 1: {}\n", input);
+    let input = args.get(1).expect("No CLI input provided.");
+    println!(
+        "Welcome to program selection!\nYour selection: {}\nProgram choices: {}\n",
+        input,
+        Program::programs().join(", ")
+    );
+
+    Program::run(String::from(input), args)
 }
---
+++ src/models/types.rs
@@ -0,0 +1,42 @@
+/// types.rs
+/// @author bradlet
+
+pub enum Program {
+    DEFAULT,
+    MAIN,
+}
+
+impl Program {
+    pub fn programs() -> Vec<Program> {
+        Vec(
+            Program::DEFAULT,
+            Program::MAIN
+        )
+    }
+
+    pub fn from(name: &str) -> Program {
+        match name {
+            "default" => Program::DEFAULT,
+            "main" => Program::MAIN,
+            _ => Program::DEFAULT,
+        }
+    }
+
+    pub fn run(name: String, args: Vec<String>) -> () {
+        println!("Running program: {}", name);
+        let used = match Program::from(&name) {
+            // In default case
+            Program::DEFAULT => Program::main( args),
+            Program::MAIN => Program::main(args),
+        };
+        println!("Completed call to {} with args: {}", name, used.join(", ").as_str())
+    }
+
+
+    pub fn main(args: Vec<String>) -> Vec<String> {
+        for arg in args.iter() {
+            println!("arg: {}\n", arg)
+        }
+        args
+    }
+}
