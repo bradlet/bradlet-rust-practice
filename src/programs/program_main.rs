8c1eab32af12298e7237254f565e78fdb30a90a5
bradlet <bthompson@tradewelltech.co>
2023-04-23 16:13:24 -0700

    Setup main to connect to local postgres db

--- src/programs/program_main.rs
+++ src/programs/program_main.rs
@@ -1,22 +1,19 @@
+use postgres::{Client, NoTls};
 /// program_main.rs
 /// @author bradlet
+use text_colorizer::Colorize;
 
 pub fn main(args: Vec<String>) -> Vec<String> {
-    println!("Welcome to Main! Running...");
-    fn take_val(str: String) -> String {
-        println!("I stole this string: {}", str);
-        str
+    let welcome = "Welcome to Main! Let's see what's in the DB...".bright_cyan();
+    println!("{}", welcome);
+
+    let mut client = Client::connect("host=localhost dbname=postgres port=32768 user=brad password=password", NoTls).unwrap();
+    for row in client.query("SELECT * FROM test;", &[]).unwrap() {
+        let name: &str = row.get(0);
+        let age: i32 = row.get(1);
+        let msg = format!("{} is {} years old!", name, age).bright_magenta();
+        println!("{}", msg);
     }
-    let test_ownership = String::from("test");
-    let returned_ownership = take_val(test_ownership);
-    println!("string is not gone? {}", returned_ownership);
-
-    let option: Option<&String> = args.get(2);
-
-    match option {
-        Some(str) => println!("You sneaky B, you included {}?!?!", str),
-        None => println!("Everything's running fine, nothing else to see here."),
-    };
 
     args
 }
