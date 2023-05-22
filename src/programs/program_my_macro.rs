c937804541d54d2337fc09aecd903262584a0c59
bradlet <bthompson@tradewelltech.co>
2023-05-21 21:43:18 -0700

    Get initial associated function impl from proc macro lib

--- src/programs/program_my_macro.rs
+++ src/programs/program_my_macro.rs
@@ -1,11 +1,26 @@
-/// program_my_macro.rs
-/// @author bradlet
-use text_colorizer::Colorize;
+//! program_my_macro.rs
+//! @author bradlet
 
+use postgres_flatten::{flattened::ToFlattenedSql, ToFlattenedSql};
+use text_colorizer::{Color, Colorize};
 
+#[derive(ToFlattenedSql)]
+struct Cat {
+    name: String,
+    age: u8,
+    color: Color,
+}
 
 pub fn main(args: Vec<String>) -> Vec<String> {
     println!("{}", "Welcome to My Macro Demo!".bright_cyan());
+
+    let test = Cat {
+        name: String::from("Rudy"),
+        age: 2,
+        color: Color::White
+    };
+
+    Cat::into_flattened_row();
 
     args
 }
