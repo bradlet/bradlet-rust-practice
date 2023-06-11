99b08be0076d3186c568c86caa4f515d0eb9c17d
bradlet <bthompson@tradewelltech.co>
2023-06-11 12:10:26 -0700

    Move database operations to program_my_macro

--- src/programs/program_main.rs
+++ src/programs/program_main.rs
@@ -1,23 +1,10 @@
-use postgres::{Client, NoTls};
 /// program_main.rs
 /// @author bradlet
 use text_colorizer::Colorize;
 
 pub fn main(args: Vec<String>) -> Vec<String> {
-    let welcome = "Welcome to Main! Let's see what's in the DB...".bright_cyan();
+    let welcome = "Welcome to Main!".bright_cyan();
     println!("{}", welcome);
-
-    let mut client = Client::connect(
-        "host=localhost dbname=postgres port=32768 user=brad password=password",
-        NoTls,
-    )
-    .unwrap();
-    for row in client.query("SELECT * FROM test;", &[]).unwrap() {
-        let name: &str = row.get(0);
-        let age: i32 = row.get(1);
-        let msg = format!("{} is {} years old!", name, age).bright_magenta();
-        println!("{}", msg);
-    }
 
     args
 }
--- src/programs/program_my_macro.rs
+++ src/programs/program_my_macro.rs
@@ -1,19 +1,39 @@
 //! program_my_macro.rs
 //! @author bradlet
 
+use postgres::{Client, NoTls};
 use postgres_flatten::{flattened::ToFlattenedSql, ToFlattenedSql};
 use text_colorizer::{Color, Colorize};
+
+#[allow(dead_code)]
+struct Health {
+    weight: f32,
+    age: u8,
+}
 
 #[allow(dead_code)]
 #[derive(ToFlattenedSql)]
 struct Cat {
     name: String,
-    age: u8,
     color: Color,
+    health_data: Health,
 }
 
 pub fn main(args: Vec<String>) -> Vec<String> {
     println!("{}", "Welcome to My Macro Demo!".bright_cyan());
+
+    let mut client = Client::connect(
+        "host=localhost dbname=postgres port=32768 user=brad password=password",
+        NoTls,
+    )
+    .unwrap();
+
+    for row in client.query("SELECT * FROM test;", &[]).unwrap() {
+        let name: &str = row.get(0);
+        let age: i32 = row.get(1);
+        let msg = format!("{} is {} years old!", name, age).bright_magenta();
+        println!("{}", msg);
+    }
 
     Cat::into_flattened_row();
 
